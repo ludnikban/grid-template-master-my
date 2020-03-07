15d57ef743a2dcf2568aeea31ed2e113884e2ef2
Liudmyla Bandalietova <ludnikban@gmail.com>
2020-03-07 16:11:41 +0200

    my first commit

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+
+  $('.header-navbar__hamburger').on('click', function () {
+
+    $(this).toggleClass('active');
+    $('.header-navbar__list').toggleClass('active');
+
+  });
+
+});
